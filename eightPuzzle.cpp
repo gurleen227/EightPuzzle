c75872054d4c1fc51ccec233c0922807efa4af3d
gurleen227 <gurleenkaurvaid@gmail.com>
2022-04-17 17:16:57 -0700

    minor changes

--- eightPuzzle.cpp
+++ eightPuzzle.cpp
@@ -187,18 +187,6 @@ void sequence(std::string s) {
 			move(s[i]);
 		}
 	}
-
-	if (!invalid) {
-		cout << "valid" << std::endl;
-	}
-	else {			
-		string input;
-		cout << "Please enter new sequence of moves:" << endl;
-		cin >> input;
-		system("cls");
-		setState(temp);
-		sequence(input);
-	}
 }		
 };
 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+if (!invalid) {
+	// 	cout << "valid" << std::endl;
+	// }
+	// else {			
+	// 	string input;
+	// 	cout << "Please enter new sequence of moves:" << endl;
+	// 	cin >> input;
+	// 	system("cls");
+	// 	setState(temp);
+	// 	sequence(input);
+	// }
